fbf9d8372440313e16cfa96759e4f1f931e5df24
wangjun <junbo.wang@autodesk.com>
2012-12-09 20:33:50 +0800

    new Xcode project

---
+++ RenLib/RenLib/RenLibAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RenLibAppDelegate.h
+//  RenLib
+//
+//  Created by junbo.wang on 12/9/12.
+//  Copyright (c) 2012 Junius. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RenLibAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
